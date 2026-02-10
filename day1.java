e4c0fee25336027bf49db54075d989eaa8e0c809
Aayush <aayush.10a2@gmail.com>
2026-02-10 10:48:43 +0530

    find second highest element code

--- day1.java
+++ day1.java
@@ -3,6 +3,20 @@ package com.DSATraining;
 public class day1 {
     public static void main(String[] args) {
         int[] arr = {-1,5,4,9,-5,2};
+        int first = -99999;
+        int second = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > first){
+                second = first;
+                first = arr[i];
+            }
+            else if (arr[i] > second && arr[i] < first) {
+                second = arr[i];
+
+            }
+        }
+        System.out.println(second);
 
     }
 
