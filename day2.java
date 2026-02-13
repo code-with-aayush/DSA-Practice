1f9a163bd2bd3139bf113461ba83923bfea5474d
Aayush <aayush.10a2@gmail.com>
2026-02-13 10:43:06 +0530

    day 2 kadane algo

---
+++ day2.java
@@ -0,0 +1,22 @@
+package com.DSATraining;
+
+public class day2 {
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int sum = 0;
+        int max = -99999;
+
+        for(int i = 0 ; i<nums.length ; i++){
+            sum += nums[i];
+
+            if(sum > max){
+                max = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+
+            }
+        }
+        System.out.println(max);
+    }
+}
