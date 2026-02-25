142c458e55bb298bc91a9a6fd0953a1cc40e5e29
Aayush <aayush.10a2@gmail.com>
2026-02-25 23:45:04 +0530

    283. Move Zeroes

--- day3_question_1.java
+++ day3_question_1.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 //283. Move Zeroes
 //Easy
 //Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-//
 //Note that you must do this in-place without making a copy of the array.
 
 
