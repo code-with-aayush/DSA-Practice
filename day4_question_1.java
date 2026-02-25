bdb3d0b9906650a9661b1e65a623726fca582486
Aayush <aayush.10a2@gmail.com>
2026-02-25 23:46:56 +0530

    Leetcode two sum using hashmap

--- day4_question_1.java
+++ day4_question_1.java
@@ -8,6 +8,25 @@ question: Leetcode two sum using hashmap.
  */
 
 
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class day4_question_1 {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int compliment = target-nums[i];
+            if (map.containsKey(compliment)){
+                System.out.print(Arrays.toString(new int[]{map.get(compliment),i}));
+                return;
+            }
+            map.put(nums[i],i);
+        }
+        System.out.println(Arrays.toString(new int[]{}));
+    }
+
 }
