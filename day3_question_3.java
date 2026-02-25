3532b33ac98026a2bbb337d58304dcbd620ae49a
Aayush <aayush.10a2@gmail.com>
2026-02-25 23:46:11 +0530

    268. Missing Number

--- day3_question_3.java
+++ day3_question_3.java
@@ -8,11 +8,35 @@ Topics
 premium lock icon
 Companies
 Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
-
-
  */
 
 
 
 public class day3_question_3 {
+    public static void main(String[] args) {
+
+
+        int[] nums = {0,3,2};
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            int correct = nums[i];
+            if (nums[i]<n && nums[i] != nums[correct]){
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            else {
+                i++;
+            }
+
+        }
+        for (int j = 0; j < n; j++) {
+            if (nums[j] != j){
+                System.out.println(j);
+                return;
+            }
+        }
+        System.out.println(nums.length);
+    }
 }
