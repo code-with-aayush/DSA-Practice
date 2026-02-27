85d804bdaae260cce38eb2e1cf406329bb66a8f6
Aayush <aayush.10a2@gmail.com>
2026-02-27 19:20:34 +0530

    128. Longest Consecutive Sequence

--- day5_question_2.java
+++ day5_question_2.java
@@ -11,5 +11,38 @@ You must write an algorithm that runs in O(n) time.
  */
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class day5_question_2 {
+    public static void main(String[] args) {
+        int[] nums = {100,4,200,1,3,2};
+        System.out.println(longestConsecutive(nums));
+    }
+
+    public static int longestConsecutive(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int i:nums){
+            set.add(i);
+        }
+
+        int longestsequence = 0;
+        for(int i : set){
+            if(!set.contains(i-1)){
+                int currentlength = 1;
+                while(set.contains(i+1)){
+                    i++;
+                    currentlength++;
+                }
+                longestsequence = Math.max(longestsequence,currentlength);
+            }
+        }
+        return longestsequence;
+    }
 }
+
