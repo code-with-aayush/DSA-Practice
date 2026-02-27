4c2fb587879d9c55d51302c46d3da6db52cbe425
Aayush <aayush.10a2@gmail.com>
2026-02-27 19:19:49 +0530

    242. Valid Anagram

--- day5_question_1.java
+++ day5_question_1.java
@@ -12,4 +12,35 @@ Given two strings s and t, return true if t is an anagram of s, and false otherw
 
 
 public class day5_question_1 {
+    public static void main(String[] args) {
+        String s = "cat";
+        String t = "rat";
+        int a = s.length();
+        int b = t.length();
+        if(a != b){
+            System.out.println(false);
+            return;
+
+        }
+        else{
+            int[] count = new int[26];
+
+            for(int i = 0; i < a; i++){
+                count[s.charAt(i) - 'a']++;
+            }
+            for(int i = 0; i < b; i++){
+                count[t.charAt(i) - 'a']--;
+            }
+            for(int i = 0; i < count.length; i++){
+                if(count[i] != 0){
+                    System.out.println(false);
+                    return;
+
+                }
+            }
+            System.out.println(true);
+            return;
+
+        }
+    }
 }
