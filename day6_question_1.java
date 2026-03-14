3138fb44c5735e5a2e85d7477ebfeb35b59991ba
Aayush <aayush.10a2@gmail.com>
2026-03-14 14:58:03 +0530

    35. search insert position

--- day6_question_1.java
+++ day6_question_1.java
@@ -10,4 +10,25 @@ You must write an algorithm with O(log n) runtime complexity.
 
 
 public class day6_question_1 {
+    public static void main(String[] args) {
+        int[] nums = {1,3,5,6};
+        int target = 5;
+        int n = nums.length;
+        int start = 0;
+        int end = n-1;
+        while(start <= end){
+            int mid = start + ((end - start)/2);
+            if(nums[mid] == target){
+                System.out.println(mid);
+                return;
+            }
+            else if(target > nums[mid]){
+                start = mid+1;
+            }
+            else{
+                end = mid -1;
+            }
+        }
+        System.out.println(start);
+    }
 }
