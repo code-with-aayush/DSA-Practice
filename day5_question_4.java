4be468b2933f52087e9cd3dda654bba6ed034768
Aayush <aayush.10a2@gmail.com>
2026-03-14 15:01:49 +0530

    560. subarray sum equals k

--- day5_question_4.java
+++ day5_question_4.java
@@ -10,6 +10,28 @@ A subarray is a contiguous non-empty sequence of elements within an array.
  */
 
 
+import java.util.HashMap;
 
 public class day5_question_4 {
+    public static void main(String[] args) {
+        int[] nums = {1,1,1};
+        int k = 2;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+
+        int result=0;
+        int prefixsum=0;
+
+        for(int num : nums){
+            prefixsum += num;
+
+            if(map.containsKey(prefixsum-k)){
+                result += map.get(prefixsum-k);
+            }
+
+            map.put(prefixsum, map.getOrDefault(prefixsum , 0) +1);
+        }
+
+        System.out.println(result);
+    }
 }
