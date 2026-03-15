78a5849c46c8b2aba2be7e26beb85a5bea865c27
Aayush <aayush.10a2@gmail.com>
2026-03-15 20:29:33 +0530

    33. Search in Rotated Sorted Array

--- day6_question_2.java
+++ day6_question_2.java
@@ -14,4 +14,33 @@ You must write an algorithm with O(log n) runtime complexity.
 
 
 public class day6_question_2{
+    public static void main(String[] args) {
+        int[] nums = {7,8,9,1,2,3,4,5,6};
+        int target = 5;
+        int n = nums.length;
+        int start = 0;
+        int end = n-1;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                System.out.println(mid);
+                return;
+            }
+            else if (nums[start] <= nums[mid]) {
+                if (target >= nums[start] && target <= nums[mid]){
+                    end = mid-1;
+                }
+                else start = mid+1;
+
+            }
+            else {
+                if (target > nums[mid] && target <= nums[end]){
+                    start = mid+1;
+                }
+                else end = mid-1;
+            }
+        }
+        System.out.println(-1);
+    }
 }
